fb9210bcc5e6f173c25cd7b0558762df5438d606
Malik Zhu <saly56642@gmail.com>
2019-08-07 01:58:09 +0800

    Raw materials

--- installserver.sh
+++ installserver.sh
@@ -6,14 +6,14 @@ wget https://github.com/creticsliu/mcserver/raw/master/server/server-icon.png
 wget https://github.com/creticsliu/mcserver/raw/master/server/bukkit.yml
 wget https://github.com/creticsliu/mcserver/raw/master/server/eula.txt
 wget https://github.com/creticsliu/mcserver/raw/master/server/paper.yml
-wget https://github.com/creticsliu/mcserver/raw/master/server/server.jar
+wget -O server.jar https://papermc.io/api/v1/paper/1.14.4/159/download
 wget https://github.com/creticsliu/mcserver/raw/master/server/server.properties
 wget https://github.com/creticsliu/mcserver/raw/master/server/spigot.yml
 wget https://github.com/creticsliu/mcserver/raw/master/native/restart.sh
 wget https://github.com/creticsliu/mcserver/raw/master/native/start.sh
 mkdir plugins
 cd plugins
-wget https://github.com/creticsliu/mcserver/raw/master/server/LaggRemover-2.0.6.jar
+wget https://media.forgecdn.net/files/2744/510/LaggRemover-2.0.6.jar
 cd /usr/mcserver
 java -jar /usr/mcserver/server.jar noggin
 echo Complete!
