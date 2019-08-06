bf292493c6a67a8cfa9f39e3572031fb8cbd92c6
Malik Zhu <saly56642@gmail.com>
2019-08-07 00:54:51 +0800

    Raw materials
    Please wait

---
+++ native/restart.sh
@@ -0,0 +1,37 @@
+echo 脚本正在运行
+while true
+do
+sleep 240
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 8 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 7 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 6 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 5 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 4 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 3 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 2 hours!\n"
+sleep 3600
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 1 hour!\n"
+sleep 3540
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 1 minute!\n"
+sleep 50
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 10 seconds!\n"
+sleep 5
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 5 seconds!\n"
+sleep 1
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 4 seconds!\n"
+sleep 1
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 3 seconds!\n"
+sleep 1
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 2 seconds!\n"
+sleep 1
+screen -dr mcserver -X stuff "say [restart.sh]The server will restart in 1 seconds!\n"
+sleep 1
+screen -dr mc -X stuff "kick @a [restart.sh]The server is currently restarting. Please wait for approximately 4 minutes before rejoining.\n"
+screen -dr mc -X stuff "stop\n"
+done
---
+++ native/start.sh
@@ -0,0 +1,4 @@
+while true
+do
+java -jar -Xmx1G /usr/mcserver/server.jar nogui
+done
