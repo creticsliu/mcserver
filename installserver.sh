e6809ccd25a5be5603add54a0240c94e5e5a6913
Malik Zhu <saly56642@gmail.com>
2019-08-07 01:15:09 +0800

    shell

---
+++ installserver.sh
@@ -0,0 +1,17 @@
+mkdir /usr/mcserver
+yum -y install java
+yum -y install wget
+yum -y install screen
+cd /usr/mcserver
+wget https://github.com/creticsliu/mcserver/raw/master/server/server-icon.png
+wget https://github.com/creticsliu/mcserver/raw/master/server/bukkit.yml
+wget https://github.com/creticsliu/mcserver/raw/master/server/eula.txt
+wget https://github.com/creticsliu/mcserver/raw/master/server/paper.yml
+wget https://github.com/creticsliu/mcserver/raw/master/server/server.jar
+wget https://github.com/creticsliu/mcserver/raw/master/server/server.properties
+wget https://github.com/creticsliu/mcserver/raw/master/server/spigot.yml
+wget https://github.com/creticsliu/mcserver/raw/master/native/restart.sh
+wget https://github.com/creticsliu/mcserver/raw/master/native/start.sh
+mkdir plugins
+cd plugins
+wget https://github.com/creticsliu/mcserver/raw/master/server/LaggRemover-2.0.6.jar 
