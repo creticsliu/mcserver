b01f87819be66845806885fa624b5f16ec3ce627
creticsliu <49654991+creticsliu@users.noreply.github.com>
2019-08-27 15:28:38 +0800

    Update installserver.sh

--- installserver.sh
+++ installserver.sh
@@ -6,7 +6,7 @@ wget https://github.com/creticsliu/mcserver/raw/master/server/server-icon.png
 wget https://github.com/creticsliu/mcserver/raw/master/server/bukkit.yml
 wget https://github.com/creticsliu/mcserver/raw/master/server/eula.txt
 wget https://github.com/creticsliu/mcserver/raw/master/server/paper.yml
-wget -O server.jar https://papermc.io/api/v1/paper/1.14.4/159/download
+wget -O server.jar https://papermc.io/api/v1/paper/1.14.4/176/download
 wget https://github.com/creticsliu/mcserver/raw/master/server/server.properties
 wget https://github.com/creticsliu/mcserver/raw/master/server/spigot.yml
 wget https://github.com/creticsliu/mcserver/raw/master/native/restart.sh
